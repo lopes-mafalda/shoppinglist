ef7189f412b2a3b85500da93db47c8ac9ad61ba0
lopes-mafalda <lopes.mafalda97@gmail.com>
2025-07-28 16:25:35 +0100

    Add simple database strucutre

---
+++ app/src/main/java/io/codeforall/android/shoppinglist/persistence/DBSchema.java
@@ -0,0 +1,16 @@
+package io.codeforall.android.shoppinglist.persistence;
+
+import android.provider.BaseColumns;
+
+public final class DBSchema {
+
+    private DBSchema() {
+    }
+
+    public static class ItemsTable implements BaseColumns {
+        public static final String TABLE = "items";
+        public static final String COL_NAME= "name";
+        public static final String COL_DESC = "description";
+
+    }
+}
